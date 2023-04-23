dac927065c1b6ea45852ca0aa410fe25053f6030
wnstn7671 <wnstn7671@naver.com>
2023-04-23 19:01:03 +0900

    230410 완료

--- src/main/java/com/example/myBlog/posts/controller/PostController.java
+++ src/main/java/com/example/myBlog/posts/controller/PostController.java
@@ -1,27 +1,43 @@
 package com.example.myBlog.posts.controller;
 
+import com.example.myBlog.posts.entity.Post;
+import com.example.myBlog.posts.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
     @GetMapping("/posts/write")
-    public String getPostWriteView()
-    {
-        return "posts/post_write";
+    public String getPostWriteView() {
+        return "/posts/post_write";
     }
+
     @PostMapping("/posts/write")
     public String postWrite(
             String title, String body
     ) {
-        System.out.println("title = " + title);
-        System.out.println("body = " + body);
+        Long savedId = postService.save(title, body);
         return "redirect:/";
     }
-    //
-    // HTTP Method
-    // GET, POST
+
+    @GetMapping("/posts/{id}")
+    public String getPost(
+            @PathVariable Long id,
+            Model model
+    ) {
+        Post findPost = postService.getById(id);
+
+        model.addAttribute("post", findPost); // Map<String, Object>
+        return "/posts/post_detail";
+    }
+
 
 }
